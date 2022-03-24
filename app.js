d2922d1e89e2a6054dc066b9b3a5a697514bd0a4
TomCevasco <tomascevasco@gmail.com>
2022-03-24 20:50:01 -0300

    Demostracion ML 54

--- app.js
+++ app.js
@@ -1,47 +1,29 @@
 const express = require("express");
 const { link } = require("fs");
 
-const app = express()
+const app = express();
 
 app.use(express.static("public"));
-const path = require("path")
+const path = require("path");
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log("running on port 3001"));
 
 
-app.listen(3001, () => {
 
-    console.log("el servidor esta corriendo en puerto 3001");
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
 
-app.get("/",(req,res) =>{
+app.get("/FormLogin", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/FormLogin.html"));
+});
 
- res.sendFile(path.resolve(__dirname, "./views/home.html")) })
+app.get("/registro", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/registro.html"));
+});
 
-
- app.get("/FormLogin",(req,res) =>{
-
-    res.sendFile(path.resolve(__dirname, "./views/FormLogin.html"))} )
-    
-    
-    
-    app.get("/Registro",(req,res) =>{
-
-        res.sendFile(path.resolve(__dirname, "./views/Registro.html"))
-        
-        }) ;
-    
-
-    
-     
-
-            app.get("/Producto",(req,res) =>{
-
-                res.sendFile(path.resolve(__dirname, "./views/Producto.html"))
-                
-                }) ;
-
-
-                
+app.get("/producto", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/producto.html"));
+});
