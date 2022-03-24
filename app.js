c0815a4d0ac13e2453392468f4af4fb8bb77a0f9
TomCevasco <tomascevasco@gmail.com>
2022-03-24 20:18:51 -0300

    Demostracion ML 2

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express()
 app.use(express.static("public"));
 const path = require("path")
 
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => console.log("running on port 3001"));
 
 
 app.listen(3001, () => {
